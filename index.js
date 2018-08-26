cd6bc75377a6f4c7665edd8a368cb39a52dcf003
Tokuhisa1 <TokuhisaWinston@Outlook.com>
2018-08-25 23:51:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array) {
+  return array.map(function (element) {
+    return element.toLowerCase();
+  });
+}
+
+function nameToAttributes(array) {
+  return array.map(function (element) {
+    // If I do not use "const"/"let", why does not adding an "s" to the end of "name" break the test?
+    const name = element.split(' ');
+    return { firstName: name[0], lastName: name[1] };
+  });
+}
+
+function attributesToPhrase(array) {
+  return array.map(function (element) {
+    return `${element.name} is from ${element.hometown}`;
+  });
+}
